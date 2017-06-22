6add407f75374470bfe55b673b37fed0fab65c0a
nidhi-p <nidhipurohit.ce@gmail.com>
2017-06-22 12:39:44 +0530

    api for health bot

---
+++ app.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") != "treatment.cost":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    zone = parameters.get("cancer_entity")
+
+    cost = {'Breast Cancer':1000, 'Prostate Cancer':1200, 'Colon Cancer':1500, 'Leukemia ':2000, 'Lung cancer':1100}
+
+    speech = "The cost of treating " + zone + " is " + str(cost[zone]) + " euros."
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "apiai-health-bot"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=True, port=port, host='0.0.0.0')
